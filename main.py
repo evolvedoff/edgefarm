f03a27e7d7670406ecfd68c4772bddb124b66d92
Evolved <91874239+evolvedoff@users.noreply.github.com>
2025-04-03 15:50:53 +0300

    Update main.py
    fixed a bug, should work fine now

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ import time
 import random
 import keyboard
 import sys
+from wonderwords import RandomWord as r
 
-def generate_random_letters():
-    letters = "abcdefghijklmnopqrstuvwxyz"
-    return ''.join(random.choices(letters, k=4))
+def generate_random_word():
+    return r().word()
 
 def search_edge(repeats, custom):
     pyautogui.hotkey('ctrl', 'e')
     if custom:
         pyautogui.typewrite(custom)
     else:
-        random_letters = generate_random_letters()
+        random_letters = generate_random_word()
         pyautogui.typewrite(random_letters + str(repeats))
+    time.sleep(0.1)
     pyautogui.press('enter')
 
 debouncerun = True
@@ -27,7 +28,7 @@ def run():
     if debouncerun and repeats >= 0:
             debouncerun = not debouncerun
             search_edge(repeats, custom=0)
-            time.sleep(random.randint(90,110) / 10)
+            time.sleep(random.randint(900, 1100) / 100)
             repeats -= 1
             debouncerun = not debouncerun
     elif repeats < 0:
